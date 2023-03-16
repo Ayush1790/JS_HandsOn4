1ea571aebb92bc166fc316ac0d69dfd435aa6c1b
Ayush1790 <ayushgupta@cedcoss.com>
2023-03-16 12:58:08 +0530

    second commit

--- src/JS/script.js
+++ src/JS/script.js
@@ -146,3 +146,114 @@ myArray = [];
 for (let i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
+//Iterate Through an Array with a For Loop
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total=0;
+for(let i=0;i<myArr.length;i++){
+  total+=myArr[i];
+}
+
+//Nesting For Loops
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    product*=arr[i][j];
+  }
+}
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+//Iterate with JavaScript Do...While Loops
+// Setup
+ myArray = [];
+ i = 10;
+
+// Only change code below this line
+ do{
+  myArray.push(i);
+  i++;
+}while (i < 10);
+
+//Replace Loops using Recursion
+function sum(arr, n) {
+  // Only change code below this line
+  if(n<=0){
+    return 0;
+  }
+  else{
+    return sum(arr,n-1)+arr[n-1];
+
+  }
+  // Only change code above this line
+}
+//Profile Lookup
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+lookUpProfile("Akira", "likes");
+
+//Generate Random Fractions with JavaScript
+function randomFraction() {
+ 
+  var result = 0;
+  
+  while (result === 0) {
+    result = Math.random();
+  }
+
+  return result;
+  // Only change code above this line.
+}
+
+//Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+
+  // Only change code below this line
+
+  return Math.floor(Math.random()*10);
+}
